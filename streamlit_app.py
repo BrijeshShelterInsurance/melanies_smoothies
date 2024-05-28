98703f4891445652084955a3222456545033b894
BrijeshShelterInsurance <bsharma@shelterinsurance.com>
2024-05-28 13:34:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 cnx = st.connection("snowflake")
@@ -21,13 +22,13 @@ ingredients_list = st.multiselect(
     "Choose up to 5 ingredients: ",my_dataframe, max_selections=5)
 
 if ingredients_list:
-    # st.write(ingredients_list)
-    # st.text(ingredients_list)
 
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
 
     # st.write(ingredients_string)
 
@@ -41,7 +42,5 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()      
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
+
+
